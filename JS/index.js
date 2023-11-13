a9cd8fad3ad53d90dbd1108911d5a6ff5943c6b8
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-11-13 19:20:47 +0200

    wip

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,15 @@
 document.getElementById("convertBtn").addEventListener("click", function () {
-    var celsius = parseFloat(document.getElementById("celsius").value);
-    var fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
-    var errorSpan = document.getElementById("error");
+    let celsius = parseFloat(document.getElementById("celsius").value);
+    let fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
+    let errorSpan = document.getElementById("error");
 
     if (!isNaN(celsius)) {
-        var f = (celsius * 9 / 5) + 32;
+        let f = (celsius * 9 / 5) + 32;
         document.getElementById("fahrenheit").value = f.toFixed(2);
         document.getElementById("result").innerHTML = celsius + "°C is " + f.toFixed(2) + "°F";
         errorSpan.innerHTML = "";
     } else if (!isNaN(fahrenheit)) {
-        var c = (fahrenheit - 32) * 5 / 9;
+        let c = (fahrenheit - 32) * 5 / 9;
         document.getElementById("celsius").value = c.toFixed(2);
         document.getElementById("result").innerHTML = fahrenheit + "°F is " + c.toFixed(2) + "°C";
         errorSpan.innerHTML = "";
@@ -18,3 +18,12 @@ document.getElementById("convertBtn").addEventListener("click", function () {
     }
 });
 
+
+document.getElementById("clearBtn").addEventListener("click", function () {
+    document.getElementById("celsius").value = "";
+    document.getElementById("fahrenheit").value = "";
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("celsius").style.border = "1px solid #ccc";
+    document.getElementById("fahrenheit").style.border = "1px solid #ccc";
+});
